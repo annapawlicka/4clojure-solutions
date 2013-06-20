2e8894c2c83ca03e79d111cdbd95ca1ff38a8960
apawlicka <pawlicka.anna@gmail.com>
2013-06-20 23:36:04 +0100

    Added problem 22 - fixed typos

--- src/easy/problem022.clj
+++ src/easy/problem022.clj
@@ -24,16 +24,21 @@
 (= ((fn [coll] (reduce (fn [n _] (inc n)) 0 coll)) '(:a :b :c)) 3)
 
 (comment
+
   Explanation
-  Reduce accepts two arguments: function f, value v and collection coll.
+
+  Reduce accepts two arguments
+  function f
+  value v
+  collection coll.
 
   Function f should accept 2 arguments. However, in this case the function will only increment the counter,
-  it will not work on the collection. Therefore our function needs only one argument: the counter. Second argument is
+  it will not work on the collection. Therefore our function needs only one argument, the counter. Second argument is
   the underscore (it is used in Clojure as a placeholder for a required but unused argument).
   (fn [n _] (inc n))
 
   Value supplied is 0. If val is supplied, reduce returns the result of applying function f to val and the first
-  item in coll, then applying f to that result and the 2nd item, etc. If coll contains no items,
+  item in coll, then applying f to that result and the second item, etc. If coll contains no items,
   returns val and f is not called.
   In this case, we use reduce for the side-effect. We do not do anything with the values of the elements in the
   collection, we simply increment the counter every time reduce moves to the next element.
