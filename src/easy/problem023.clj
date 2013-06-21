d117d3b2dedc5e8169d5fa116c389dfc0b27c918
apawlicka <pawlicka.anna@gmail.com>
2013-06-21 12:43:00 +0100

    Added problem 23 - alternative solution

--- src/easy/problem023.clj
+++ src/easy/problem023.clj
@@ -21,6 +21,7 @@
         (recur (rest r) (conj result (first r))))))
   )
 
+
 ;; Code:
 (= ((fn [coll]
   (loop [r (rest coll) result (conj () (first coll))]
@@ -38,16 +39,28 @@
           result
           (recur (rest r) (conj result (first r)))))) [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
 
+;; Answer 2:
+#(reduce conj () %)
+
+;; Code 2:
+(= (#(reduce conj () %) [1 2 3 4 5]) [5 4 3 2 1])
+(= (#(reduce conj () %) (sorted-set 5 7 2 7)) '(7 5 2))
+(= (#(reduce conj () %)  [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
 
 (comment
 
   Explanation
 
+  Solution 1
   Recursively looping through items in a sequence (loop acts as a recur target).
   Loop uses two bindings
   r - the rest of the sequence
   result - first element of the sequence added to the new sequence (conj'ed).
   If there are no more elements in the sequence (empty? r), the new sequence is returned.
   Otherwise, recur executes loop again with updated bindings.
+
+  Solution 2
+  Much shorter higher-order reduce that uses conj as a function and adds each element to
+  the new sequence.
 
   )
