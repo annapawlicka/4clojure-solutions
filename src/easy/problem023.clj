741c3e9f2dc3ec64489c22bee8171a162f42f04d
apawlicka <pawlicka.anna@gmail.com>
2013-06-21 12:37:02 +0100

    Added problem 23

---
+++ src/easy/problem023.clj
@@ -0,0 +1,53 @@
+;; http://www.4clojure.com/problem/23
+
+;; Reverse a Sequence
+;; Difficulty:	Easy
+;; Topics: seqs core-functions
+
+;; Write a function which reverses a sequence.
+;; Special restrictions: reverse rseq
+
+;; (= (__ [1 2 3 4 5]) [5 4 3 2 1])
+;; (= (__ (sorted-set 5 7 2 7)) '(7 5 2))
+;; (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+
+
+;; Answer:
+(comment
+  (fn [coll]
+    (loop [r (rest coll) result (conj () (first coll))]
+      (if (empty? r)
+        result
+        (recur (rest r) (conj result (first r))))))
+  )
+
+;; Code:
+(= ((fn [coll]
+  (loop [r (rest coll) result (conj () (first coll))]
+    (if (empty? r)
+      result
+      (recur (rest r) (conj result (first r)))))) [1 2 3 4 5]) [5 4 3 2 1])
+(= ((fn [coll]
+      (loop [r (rest coll) result (conj () (first coll))]
+        (if (empty? r)
+          result
+          (recur (rest r) (conj result (first r)))))) (sorted-set 5 7 2 7)) '(7 5 2))
+(= ((fn [coll]
+      (loop [r (rest coll) result (conj () (first coll))]
+        (if (empty? r)
+          result
+          (recur (rest r) (conj result (first r)))))) [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+
+
+(comment
+
+  Explanation
+
+  Recursively looping through items in a sequence (loop acts as a recur target).
+  Loop uses two bindings
+  r - the rest of the sequence
+  result - first element of the sequence added to the new sequence (conj'ed).
+  If there are no more elements in the sequence (empty? r), the new sequence is returned.
+  Otherwise, recur executes loop again with updated bindings.
+
+  )
